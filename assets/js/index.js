7359709229869b021822a93167af7697bbd0d905
Pao <ma.paola.tapia@gmail.com>
2023-02-14 23:20:06 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+// const toggleButton = document.querySelector(".menu-toggle");
+// const menu = document.querySelector(".menu");
+
+// toggleButton.addEventListener("click", function () {
+//     menu.classList.toggle("visible");
+// });
