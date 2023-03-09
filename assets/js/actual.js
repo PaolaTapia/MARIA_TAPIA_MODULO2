897150d202a09e0d67ec021be4c9111d7e3a4375
Pao <ma.paola.tapia@gmail.com>
2023-03-09 18:11:21 -0300

    busqueda 1

--- assets/js/actual.js
+++ assets/js/actual.js
@@ -1,5 +1,57 @@
-
+//MUESTRA TODOS LOS EVENTOS
 let tarjetas = document.getElementById("cards")
+tarjetas.innerHTML = htmlEvents
 
+//RENDERIZA TODAS LAS CATEGORIAS
+let categorias = document.getElementById("categorias")
+categorias.innerHTML = htmlCategories
 
-tarjetas.innerHTML += `${htmlEvents}`
+//FILTRO POR CATEGORIAS
+let itemCategorias = document.querySelectorAll('input[type=checkbox]')
+
+let selected = []
+let eventsCategorizados = []
+
+itemCategorias.forEach(listItem => listItem.onclick = () => {
+    let categoria = listItem.value;
+    let categoriaClickeada = listItem.checked;
+
+    if (categoriaClickeada === true) {
+        let categoriaElegida = eventsPast.filter(event => event.category === categoria)
+
+        if (!selected.includes(categoriaElegida)) selected.push(categoriaElegida)
+
+    } else {
+        let categoriaElegida = eventsPast.filter(event => event.category === categoria)
+        selected.pop(categoriaElegida)
+    }
+
+    let htmlResultadoPorCategoria = "";
+    for (let event of selected) {
+        eventsCategorizados.push(event)
+        htmlResultadoPorCategoria += crearCard(event[0]);
+    }
+
+    if (selected.length === 0)
+        tarjetas.innerHTML = htmlEvents
+    else
+        tarjetas.innerHTML = htmlResultadoPorCategoria
+}
+)
+
+//FILTRO INPUT SEARCH
+let inputSearch = document.querySelector('input[type="search"]')
+const log = document.getElementById("log");
+inputSearch.addEventListener("keyup", buscar);
+
+var textoBusqueda = ""
+var resultado = ""
+function buscar(e) {
+
+    textoBusqueda = inputSearch.value;
+    console.log('%c :::VER::::', 'background: yellow; color: black', eventsCategorizados)
+    console.log('%c :::VER::::', 'background: yellow; color: black', eventsPast)
+    eventsPast.filter(event => event.name === textoBusqueda)
+}
+
+//onkeyup="buscar()"
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,78 @@
 let htmlEventsPast = "";
 let htmlEventsFuture = "";
 let htmlEvents = "";
+let htmlCategories = "";
+let eventsPast = [];
+let eventsFuture = [];
 // console.log('data', data)
-for (let event of data.events) {
-    let currentDate = new Date(data.currentDate);
-    let eventDate = new Date(event.date);
 
-    if (eventDate < currentDate) {
-        htmlEventsPast += `<div class="card mt-3" style="width: 18rem; height: 25rem">
-        <img
-          src="${event.image}"
-          class="card-img-top"
-          alt="..."
-        />
-        <div class="card-body">
-          <h5 class="card-title">${event.name}</h5>
-          <p class="card-text">
-          ${event.description}
-          </p>
-          <a href="./details.html" class="btn btn-primary">More info...</a>
-        </div>
-      </div>`;
-    } else if (eventDate > currentDate) {
-        htmlEventsFuture += `<div class="card mt-3" style="width: 18rem; height: 25rem">
-        <img
-          src="${event.image}"
-          class="card-img-top"
-          alt="..."
-        />
-        <div class="card-body">
-          <h5 class="card-title">${event.name}</h5>
-          <p class="card-text">
-          ${event.description}
-          </p>
-          <a href="./details.html" class="btn btn-primary">More info...</a>
-        </div>
-      </div>`;
-    }
-
-    htmlEvents += `<div class="card mt-3" style="width: 18rem; height: 25rem">
-        <img
-          src="${event.image}"
-          class="card-img-top"
-          alt="..."
-        />
-        <div class="card-body">
-          <h5 class="card-title">${event.name}</h5>
-          <p class="card-text">
-          ${event.description}
-          </p>
-          <a href="./details.html" class="btn btn-primary">More info...</a>
-        </div>
-      </div>`;
+function crearCard(event) {
+  return ` <div class="card mt-3" style="width: 18rem; height: 25rem">
+      <img
+        src="${event.image}"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title">${event.name}</h5>
+        <p class="card-text">
+        ${event.description}
+        </p>
+        <a href="./details.html" class="btn btn-primary">More info...</a>
+      </div>
+    </div>`
 }
+
+function crearCategory(category) {
+  return `<label class="list-group-item d-flex gap-2">
+  <input
+    class="form-check-input flex-shrink-0"
+    type="checkbox"
+    value="${category}"
+  />
+  <span> ${category}</span>
+</label>`
+}
+
+// $('.navbar-toggler').click(function () {
+//   $('.navbar-toggler-icon').toggleClass('active');
+// });
+
+const menuToggle = document.getElementById('menu-toggle');
+const navbarCollapse = document.getElementById('navbarNav');
+
+menuToggle.addEventListener('click', () => {
+  navbarCollapse.classList.toggle('show');
+});
+
+
+
+let categories = []
+
+for (let event of data.events) {
+  let currentDate = new Date(data.currentDate);
+  let eventDate = new Date(event.date);
+
+  if (eventDate < currentDate) {
+    htmlEventsPast += crearCard(event);
+    eventsPast.push(event)
+  } else if (eventDate > currentDate) {
+    htmlEventsFuture += crearCard(event);
+    eventsFuture.push(event)
+  }
+
+  htmlEvents += crearCard(event);
+
+  if (!categories.includes(event.category)) {
+    categories.push(event.category)
+    htmlCategories += crearCategory(event.category);
+  }
+
+
+}
+
 // console.log(htmlEventsPast);
+// console.log(htmlCategories);
 // console.log(htmlEventsFuture);
 // console.log(htmlEvents);
 
